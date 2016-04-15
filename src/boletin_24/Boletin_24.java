9e236ff6bcbeebfe529741027449737b5049b274
MATE <MATE@MATE-PC>
2016-04-15 12:05:36 +0200

    INICIO

---
+++ src/boletin_24/Boletin_24.java
@@ -0,0 +1,12 @@
+package boletin_24;
+
+public class Boletin_24 {
+
+    public static void main(String[] args) {
+       Ventana_1 obx = new Ventana_1();
+       Ventana_2 vent = new Ventana_2();
+       obx.setVisible(true);
+       vent.setVisible(true);
+    }
+    
+}
